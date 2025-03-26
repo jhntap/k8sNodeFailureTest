447d467bf65283e891a7e517d28b3e90bc3d7e93
Jung <jihoonj@netapp.com>
2025-03-26 15:22:43 +0900

    update command

--- verify_status.sh
+++ verify_status.sh
@@ -1,3 +1,11 @@
-kubectl get deployment -o wide
-kubectl get rs -o wide
-kubectl get pod -o wide
+#!/bin/bash
+echo "kubectl get $(kubectl get deployment -o name) -o wide"
+kubectl get $(kubectl get deployment -o name) -o wide
+echo "kubectl get $(kubectl get rs -o name) -o wide"
+kubectl get $(kubectl get rs -o name) -o wide
+echo "kubectl get $(kubectl get pod -o name) -o wide"
+kubectl get $(kubectl get pod -o name) -o wide
+echo "kubectl get $(kubectl get pvc -o name) -o wide"
+kubectl get $(kubectl get pvc -o name) -o wide
+echo "kubectl get $(kubectl get volumeattachments -o name) -o wide"
+kubectl get $(kubectl get volumeattachments -o name) -o wide
